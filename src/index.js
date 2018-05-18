0a6e74931b6077db32f6099d6ac92667690f1aec
samhhollon <samhhollon@gmail.com>
2018-05-17 19:49:21 -0500

    Finished adding temporary authentication

--- src/index.js
+++ src/index.js
@@ -4,17 +4,30 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import registerServiceWorker from './registerServiceWorker'
 
+import { ApolloClient, HttpLink, InMemoryCache, ApolloLink } from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
-import { ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost'
 
+import { AUTH_TOKEN } from './constants'
 import App from './components/App'
 
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 
